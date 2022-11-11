943f79e02e2780b9ba2eeb1ff9e5fe2bc9b58be1
Nikita <61729611+KotikNikita@users.noreply.github.com>
2022-11-11 15:28:00 +0300

    Update models.py

--- exps/models.py
+++ exps/models.py
@@ -1,6 +1,8 @@
 from .lobert_utils import lobert_student
 from .dummy_model import dummy_func
 from .head_pruning import random_head_pruning_model
+from .svd_bert_model import svd_bert
+from .bert_pruning_l1 import bert_prune
 
 from collections import OrderedDict
 MODEL_NAMES = OrderedDict(
@@ -10,6 +12,8 @@ MODEL_NAMES = OrderedDict(
         ("random_head", "random_head_pruning"),
         ("svd_selfatt", "lobert_self_svd"),
         ("svd_fnn", "lobert_self_ffn"),
+        ("pruning_l1", "pruning_l1_self"),
+        ("svd_bert", "svd_bert_self"),
     ])
 
 
@@ -34,3 +38,11 @@ def lobert_self_ffn(model):
         student_mode = "ffn_svd", 
         num_hidden_layers = model.config.num_hidden_layers)
     return model
+    
+def pruning_l1_self(model):
+    mdl = bert_prune(model)
+    return mdl
+    
+def svd_bert_self(model):
+    mdl = svd_bert(model)
+    return mdl
