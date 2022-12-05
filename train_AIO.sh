0354098931feff68253df71022907825b85ba717
Seleznyov Mikhail <simplymike8@gmail.com>
2022-12-05 16:42:39 +0300

    Change script to run TTm experiment

--- train_AIO.sh
+++ train_AIO.sh
@@ -1,17 +1,17 @@
 for model in bert-base-uncased
 do
    echo "doing $model "
-   CUDA_VISIBLE_DEVICES=0 python bench_glue_AIO.py \
+   CUDA_VISIBLE_DEVICES=5 python bench_glue_AIO.py \
 	--model_name_or_path $model \
-	--run_name $model-bert-3epoch_our_fnn600 --exp_name 'our_fnn' --rank 600 \
+	--run_name $model-bert-3epoch_ttm_ffn_full --exp_name 'ttm_ffn' \
 	--save_strategy "epoch" \
 	--logging_strategy no --save_strategy no \
 	--do_bench --bench_on_eval --bench_on_train \
-	--batch_sizes [1,16,32] \
+	--batch_sizes [1,16] \
 	--sequence_lengths [128] \
 	--max_bench_iter 1 \
 	--max_seq_length 128 \
-	--per_device_train_batch_size 32 \
+	--per_device_train_batch_size 16 \
 	--per_device_eval_batch_size 1 \
 	--learning_rate 5e-5 \
 	--num_train_epochs 3 \
