77877f644cfc5140fef1a162f5c9c1380e0ec9fc
Seleznyov Mikhail <simplymike8@gmail.com>
2023-01-12 14:10:08 +0300

    Reorganize scripts

--- scripts/train_AIO.sh
+++ scripts/train_AIO.sh
@@ -1,9 +1,10 @@
-for model in bert-base-uncased
+for rank in 150
 do
-   echo "doing $model "
+   echo "doing $model"
    CUDA_VISIBLE_DEVICES=5 python bench_glue_AIO.py \
-	--model_name_or_path $model \
-	--run_name $model-bert-3epoch_ttm_ffn_full --exp_name 'ttm_ffn' \
+	--model_name_or_path bert-base-uncased \
+	--run_name gather_fisher_matrices_cola --exp_name 'fwsvd_ffn' --rank $rank \
+	--low_rank_method "anderson" \
 	--save_strategy "epoch" \
 	--logging_strategy no --save_strategy no \
 	--do_bench --bench_on_eval --bench_on_train \
@@ -11,13 +12,13 @@ do
 	--sequence_lengths [128] \
 	--max_bench_iter 1 \
 	--max_seq_length 128 \
-	--per_device_train_batch_size 16 \
+	--per_device_train_batch_size 64 \
 	--per_device_eval_batch_size 1 \
 	--learning_rate 5e-5 \
 	--num_train_epochs 3 \
 	--evaluation_strategy 'epoch' \
 	--seed 42 \
-	--output_dir ./data_eval_v2/ \
+	--output_dir ./nla_proj_results/ \
 	--overwrite_output_dir \
 	--do_train --do_eval \
 #	--fp16 --fp16_full_eval 
---
+++ scripts/weighted_train_AIO.sh
@@ -0,0 +1,28 @@
+for method in "weighted-svd"
+do
+    for rank in 150 300 450 600
+    do
+        echo "doing $method rank $rank "
+        CUDA_VISIBLE_DEVICES=3 python bench_glue_AIO.py \
+        --model_name_or_path bert-base-uncased \
+        --run_name bert-base-uncased-3epoch_fwsvd_${method}_${rank}_cola --exp_name 'fwsvd_ffn' --rank $rank \
+        --low_rank_method ${method} \
+        --save_strategy "epoch" \
+        --logging_strategy no --save_strategy no \
+        --do_bench --bench_on_eval --bench_on_train \
+        --batch_sizes [1,16] \
+        --sequence_lengths [128] \
+        --max_bench_iter 1 \
+        --max_seq_length 128 \
+        --per_device_train_batch_size 64 \
+        --per_device_eval_batch_size 1 \
+        --learning_rate 5e-5 \
+        --num_train_epochs 3 \
+        --evaluation_strategy 'epoch' \
+        --seed 49 \
+        --output_dir ./nla_proj_results/ \
+        --overwrite_output_dir \
+        --do_train --do_eval \
+    #	--fp16 --fp16_full_eval 
+    done
+done
