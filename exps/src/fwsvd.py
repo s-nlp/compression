22878c6a9af1b398088d0a6bc2122f3d8f10f30b
Seleznyov Mikhail <simplymike8@gmail.com>
2023-04-06 14:34:01 +0300

    minor changes

--- exps/src/fwsvd.py
+++ exps/src/fwsvd.py
@@ -126,8 +126,8 @@ def estimate_fisher_weights_bert(
             fisher_int[i] += grad_int
             fisher_out[i] += grad_out
 
-    fisher_int = dict(map(lambda x: (x[0], x[1] / x[1].max()), fisher_int.items()))
-    fisher_out = dict(map(lambda x: (x[0], x[1] / x[1].max()), fisher_out.items()))
+    #fisher_int = dict(map(lambda x: (x[0], x[1] / x[1].max()), fisher_int.items()))
+    #fisher_out = dict(map(lambda x: (x[0], x[1] / x[1].max()), fisher_out.items()))
 
     #for key, item in fisher_int.items():
     #    np.save(f"fisher_matrix_inter_layer_{key}.npy", item.detach().cpu().numpy())
